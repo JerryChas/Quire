9f3c3d56b569064c4f65ba860c6624817a5d09c1
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-01-25 08:50:49 +0100

    basig layout for the note editor

--- js/editor.js
+++ js/editor.js
@@ -1,4 +1,23 @@
 //Funktion för att rendera ut notes i containern i main
+
+
+//============================================
+//Calling the render funktion with dubby data:
+//This is dummy data just for development state:
+
+
+/* renderNotesMain({
+    title: 'Första inlägget',
+    id: 99991,
+    dateCreated: '2022-01-01',
+    dateLastEdited: '2022-01-05',
+    bodyText:
+        'Det här är det första inlägget i min blogg. Jag började skriva för att dela med mig av mina tankar, erfarenheter och äventyr. Det känns fantastiskt att ha denna möjlighet att kommunicera med er läsare.',
+}); */
+
+
+//============================================
+
 function renderNotesMain(note) {
 
     console.log("vi är här")
