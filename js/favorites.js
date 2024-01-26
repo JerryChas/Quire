1dbcd9efb48835faf618d90f6e9f36a1d0c45b7b
Alicia <aliciaxd@live.se>
2024-01-26 16:58:04 +0100

    finished function to push favorites to localstorage

--- js/favorites.js
+++ js/favorites.js
@@ -1,74 +1,22 @@
-function favorite(){
-  
-    //Gets the container from HTML
-  const displayContainer = document.getElementById("display_container");
-
-  const starButton = document.getElementById("favourite-icon");
-
-  //Creates an event on click
-  starButton.addEventListener("click", toggleFavorite);
-
-  //Toggles the favorite notes
-  function toggleFavorite() {
-    note.isFavourite = !note.isFavourite;
-
-    saveToLocalStorage(note);
-
-    renderFavorites();
-  }
+function favorite(changedNote){
 
   //Saves the notes to local storage
   function saveToLocalStorage() {
     const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    console.log(changedNote);
+    console.log('fghvb');
+    // If the note is a favorite, it gets pushed to the favorites
+    if (changedNote.isFavourite == true) {
+      favorites.push(changedNote);
 
-    //If the note is a favorite, it gets pushed to the favorites
-    if (note.isFavourite) {
-      favorites.push(note);
-
-      //Otherwise it removes it from favorites
+    //   //Otherwise it removes it from favorites
     } else {
-      localStorage.setItem(
-        "favorites",
-        JSON.stringify(favorites.filter((favNote) => favNote.id !== note.id))
-      );
+      localStorage.setItem("favorites", JSON.stringify(favorites.filter((favNote) => favNote.id !== changedNote.id)));
       return;
     }
 
     //Saves the favorites to localstorage
     localStorage.setItem("favorites", JSON.stringify(favorites));
   }
-
-  function renderFavorites() {
-    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-
-    //Displays favorites on the page
-    if (favorites.length > 0) {
-      displayContainer.innerHTML = `
-            <h4 id="favorites"> Dina favoriter </h4>
-            <div class="your_favorites"> </div>
-            `;
-
-      const favoritesContainer = document.querySelector(".your_favorites");
-
-      //For each favorite note, it creates a div with title and content
-      favorites.forEach((favNote) => {
-        const favoriteNote = document.createElement("div");
-        favoriteNote.classList.add("favorite-note");
-
-        favoriteNote.innerHTML = `
-                <h5>${favNote.title}</h5>
-                <p id="fav_content">${favNote.content}</p>
-                `;
-
-        favoritesContainer.appendChild(favoriteNote);
-      });
-
-      // If you don't have a favorite, this renders
-    } else {
-      displayContainer.innerHTML = `
-            <h4 id="favorites">Dina favoriter</h4>
-            <div class="no_favorites"> Du har inga favoriter. </div>
-            `;
-    }
-  }
+  saveToLocalStorage();
 }
