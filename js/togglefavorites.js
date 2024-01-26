353cbba2b097f0058a5c8325ab6d806a682af4c3
Alicia <aliciaxd@live.se>
2024-01-26 15:17:31 +0100

    toggle favorite-status

---
+++ js/togglefavorites.js
@@ -0,0 +1,23 @@
+function toggleFavorite(noteObject) {
+    const starButton = document.getElementById("favourite-icon");
+
+    // Creates click event when clicked and toggles status of isFav
+    starButton.addEventListener("click", function () {
+        console.log(noteObject);
+
+        let changedNote = notes.find((note) => {
+            return note.id == noteObject.id;
+        });
+
+        console.log(changedNote);
+
+        if (changedNote) {
+            //Checking if changedNote is undefined
+            changedNote.isFavourite = !changedNote.isFavourite;
+            console.log(notes);
+            console.log(changedNote);
+        } else {
+            console.error("Note not found");
+        }
+    });
+}
