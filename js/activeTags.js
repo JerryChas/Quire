2d895aaba3c56eb023d09248068b0560da636a2a
Moa Sandberg <moisan1999@gmail.com>
2024-02-07 12:00:22 +0100

    resolved some issues

--- js/activeTags.js
+++ js/activeTags.js
@@ -8,17 +8,19 @@ Plocka fram alla tags ur objektet men inte upprepa dubbletter
 objekt = note.tags
 --------------------------------------------------------------*/ 
 
-const activeTagSection = document.getElementById('active-tag_section');
+const activeTagUl = document.getElementById('active-tag_ul');
+
 
 //function for displaying and going through the active tags
 const displayactiveTags = () => {
-
+console.log('hej');
     allTheTags = notes.reduce((acc, note)=> {
         /*checks if its an array it runs through and also so that the objects has the tags property, otherwise it skips the object*/
         if (note.tags && Array.isArray(note.tags)) {
             note.tags.forEach(tag => {
                 if (!acc.includes(tag)){
-                    acc.push(tag);console.log(hej)
+                    acc.push(tag);
+                    
                 }   
             });
         }
@@ -27,9 +29,8 @@ const displayactiveTags = () => {
     // Functionality for rendering the all the tags in a html-li element
     allTheTags.forEach(tag => {
         const tagsLi = document.createElement('li');
-        li.textContent = tag;
-        activeTagSection.appendChild(tagsLi);
-        
+        tagsLi.textContent = tag;
+        activeTagUl.appendChild(tagsLi);
     });
 };
 
