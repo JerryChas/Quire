489cda2e901ed96702c10eb43f86f4d34d9588c8
Moa Sandberg <moisan1999@gmail.com>
2024-02-08 10:24:53 +0100

    fixed the remaining issues

--- js/activeTags.js
+++ js/activeTags.js
@@ -10,23 +10,27 @@ objekt = note.tags
 
 const activeTagUl = document.getElementById('active-tag_ul');
 
-
 //function for displaying and going through the active tags
 const displayactiveTags = () => {
-console.log('hej');
-    allTheTags = notes.reduce((acc, note)=> {
+
+// console.log('når den denna?');
+activeTagUl.innerHTML = '';
+
+    const allTheTags = notes.reduce((acc, note)=> {
         /*checks if its an array it runs through and also so that the objects has the tags property, otherwise it skips the object*/
         if (note.tags && Array.isArray(note.tags)) {
             note.tags.forEach(tag => {
                 if (!acc.includes(tag)){
                     acc.push(tag);
-                    
-                }   
+                };   
             });
         }
         return acc;
     }, []);
     // Functionality for rendering the all the tags in a html-li element
+    // console.log('tags:', allTheTags.length);
+    // console.log('Notes array:', notes);
+    
     allTheTags.forEach(tag => {
         const tagsLi = document.createElement('li');
         tagsLi.textContent = tag;
@@ -34,10 +38,5 @@ console.log('hej');
     });
 };
 
-displayactiveTags();
-//const allTheTags = [];
-// notes.forEach(note => {
-//     if (note.tags && Array.isArray(note.tags)){
-//         allTheTags.push(...note.tags);
-//     }
-// });
+window.onload = displayactiveTags;
+
