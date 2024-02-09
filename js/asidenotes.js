3063e49731fcad57ae908d41751253e64158261e
jerryChas <jerry.olsson@chasacademy.se>
2024-02-09 14:07:57 +0100

    deleted old dummys

--- js/asidenotes.js
+++ js/asidenotes.js
@@ -1,18 +1,4 @@
-//  let notes = [
-//     {
-//         title: "titel1",
-//         id: 1,
-//         dateCreated: "1995-12-25",
-//         dateLastEdited: "1995-12-25",
-//         bodyText: "lorem ipsum jadi jadi 1"
-//     },
-//     {
-//         title: "titel2",
-//         id: 2,
-//         dateCreated: "1995-12-26",
-//         dateLastEdited: "1995-12-26",
-//         bodyText: "lorem ipsum jadi jadi 2"
-//     },
+
 
 const notesListUl = document.getElementById('notes-list_ul');
 
