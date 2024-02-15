9b39b3cfd9bed548377dd9c89452134aef53df5a
Moa Sandberg <moisan1999@gmail.com>
2024-02-15 10:46:01 +0100

    added the edit button back since i forgot i removed it

--- js/displaySearchResults.js
+++ js/displaySearchResults.js
@@ -91,6 +91,7 @@ searchResultContainer.addEventListener('click', (event) => {
             <h2>${noteToPreview.title}</h2>
             <p class="date-information"> Created ${noteToPreview.dateCreated} | Last edited ${noteToPreview.dateLastEdited}</p>
             <p id="preview-body-text">${noteToPreview.bodyText}</p>
+            <button class="button">Edit</button>
         </div>
     </div>`
 
